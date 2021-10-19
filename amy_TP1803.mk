b1dff0495cdb42a8d0be5639b1b8f45c87cdcf8b
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2021-10-19 09:49:13 -0400

    TP1803: Merge with main tree from Pixel Experience
    * Just to prevent roomservice getting confused.
    
    Co-authored-by: Wind/owZ <windowzytch@amyrom.ml>
    Signed-off-by: Wind/owZ <windowz414@gnuweeb.org>

--- amy_TP1803.mk
+++ amy_TP1803.mk
@@ -6,14 +6,14 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LOS stuff.
+# Inherit some common amyROM stuff.
 $(call inherit-product, vendor/amy/config/common_full_phone.mk)
-
-# Inherit our Lineage additions
-$(call inherit-product, device/nubia/TP1803L/device-lineage.mk)
 
 # Inherit from TP1803 device
 $(call inherit-product, device/nubia/TP1803/device.mk)
+
+# amyROM-specific flags
+TARGET_USES_BLUR := true
 
 PRODUCT_BRAND := Nubia
 PRODUCT_DEVICE := TP1803
@@ -21,5 +21,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-nubia
 PRODUCT_MANUFACTURER := Nubia
 PRODUCT_NAME := amy_TP1803
 PRODUCT_MODEL := Mini 5G
-# additional Amy stuff 
-TARGET_USES_BLUR := true
